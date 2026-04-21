0fdf58054c6c3e02f7477e4864da870378de3294
JuanDiCV <n00366753@upn.pe>
2026-04-21 17:53:34 -0500

    Modelo Citas

---
+++ src/main/java/com/heavylink/model/Citas.java
@@ -0,0 +1,43 @@
+package com.heavylink.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
+public class Citas {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Integer idCita;
+
+    @Column(nullable = false, length = 100)
+    private String nombreCliente;
+
+    @Column(nullable = false, length = 100)
+    private String abogadoAsignado;
+
+    @Column(nullable = false, length = 150)
+    private String asuntoLegal; // Ej: "Consulta de divorcio", "Revisión de contrato", etc.
+
+    @Column(nullable = true, length = 500)
+    private String detallesAdicionales; // Opcional: para que el asistente o cliente deje notas
+
+    @Column(nullable = false)
+    private LocalDateTime fechaHora; // Indispensable para saber cuándo es la cita
+
+    @Column(nullable = false)
+    private boolean activa; // true = Pendiente/Confirmada, false = Cancelada o Finalizada
+}
